f28f1da01cc08c569175c0e353490d7ae22a046a
MS-Teja <sivatejamutyala@gmail.com>
2024-06-21 19:49:13 +0530

    1.0

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push({ text: this.newTask, completed: false });
+        this.newTask = '';
+      }
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    toggleCompletion(index) {
+      this.tasks[index].completed = !this.tasks[index].completed;
+    }
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Add a new task" />
+    <button @click="addTask">Add Task</button>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <span :class="{ completed: task.completed }" @click="toggleCompletion(index)">{{ task.text }}</span>
+        <button @click="deleteTask(index)">Delete</button>
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.completed {
+  text-decoration: line-through;
 }
 </style>
